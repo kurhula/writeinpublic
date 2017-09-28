ciudadanointeligente/write-it

Hide unmoderated messages in all messages from view

nuntium/tests/see_all_messages_from_a_person_tests.py
@@ -2,9 +2,9 @@
 from global_test_case import GlobalTestCase as TestCase
 from instance.models import WriteItInstance
 from nuntium.models import Message
+from popit.models import Person
 from subdomains.utils import reverse
 from django.utils.unittest import skip
-
 
 class SeeAllMessagesFromAPersonTestCase(TestCase):
     def setUp(self):
@@ -120,3 +120,51 @@ class SeeAllMessagesFromAPersonTestCase(TestCase):
             'message_slug': 'i-am-a-slug-and-i-do-not-exist',  # This is the problem
             })
         self.assertEquals(self.client.get(url).status_code, 404)
+
+    def test_unmoderated_messages_not_included(self):
+        recipient = Person.objects.first()
+        first = Message.objects.create(
+            writeitinstance=self.writeitinstance,
+            public=True,
+            confirmated=True,
+            moderated=True,
+            subject='m: yes, p: yes, c: yes',
+            slug='m:yes, p:yes, c:yes',
+            content='example content',
+            author_name='joe bloggs',
+            author_email='joe@example.com',
+            persons=[recipient],
+        )
+        Message.objects.create(
+            writeitinstance=self.writeitinstance,
+            public=True,
+            confirmated=True,
+            moderated=None,
+            subject='m: null, p: yes, c: yes',
+            slug='m:null, p:yes, c:yes',
+            content='example content',
+            author_name='joe bloggs',
+            author_email='joe@example.com',
+            persons=[recipient],
+        )
+        Message.objects.create(
+            writeitinstance=self.writeitinstance,
+            public=True,
+            confirmated=True,
+            moderated=False,
+            subject='m: no, p: yes, c: yes',
+            slug='m:no, p:yes, c:yes',
+            content='example content',
+            author_name='joe bloggs',
+            author_email='joe@example.com',
+            persons=[recipient],
+        )
+        url = reverse('all-messages-from-the-same-author-as', subdomain=self.message.writeitinstance.slug,
+            kwargs={
+                'message_slug': first.slug
+            })
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertIn('m: yes, p: yes, c: yes', response.content)
+        self.assertIn('m: null, p: yes, c: yes', response.content)
+        self.assertNotIn('m: no, p: yes, c: yes', response.content)

kurhula/writeinpublic

nuntium/tests/see_all_messages_from_a_person_tests.py
@@ -2,9 +2,9 @@
 from global_test_case import GlobalTestCase as TestCase
 from instance.models import WriteItInstance
 from nuntium.models import Message
+from popolo.models import Person
 from subdomains.utils import reverse
 from django.utils.unittest import skip
-
 
 class SeeAllMessagesFromAPersonTestCase(TestCase):
     def setUp(self):
@@ -120,3 +120,51 @@ class SeeAllMessagesFromAPersonTestCase(TestCase):
             'message_slug': 'i-am-a-slug-and-i-do-not-exist',  # This is the problem
             })
         self.assertEquals(self.client.get(url).status_code, 404)
+
+    def test_unmoderated_messages_not_included(self):
+        recipient = Person.objects.first()
+        first = Message.objects.create(
+            writeitinstance=self.writeitinstance,
+            public=True,
+            confirmated=True,
+            moderated=True,
+            subject='m: yes, p: yes, c: yes',
+            slug='m:yes, p:yes, c:yes',
+            content='example content',
+            author_name='joe bloggs',
+            author_email='joe@example.com',
+            persons=[recipient],
+        )
+        Message.objects.create(
+            writeitinstance=self.writeitinstance,
+            public=True,
+            confirmated=True,
+            moderated=None,
+            subject='m: null, p: yes, c: yes',
+            slug='m:null, p:yes, c:yes',
+            content='example content',
+            author_name='joe bloggs',
+            author_email='joe@example.com',
+            persons=[recipient],
+        )
+        Message.objects.create(
+            writeitinstance=self.writeitinstance,
+            public=True,
+            confirmated=True,
+            moderated=False,
+            subject='m: no, p: yes, c: yes',
+            slug='m:no, p:yes, c:yes',
+            content='example content',
+            author_name='joe bloggs',
+            author_email='joe@example.com',
+            persons=[recipient],
+        )
+        url = reverse('all-messages-from-the-same-author-as', subdomain=self.message.writeitinstance.slug,
+            kwargs={
+                'message_slug': first.slug
+            })
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertIn('m: yes, p: yes, c: yes', response.content)
+        self.assertIn('m: null, p: yes, c: yes', response.content)
+        self.assertNotIn('m: no, p: yes, c: yes', response.content)