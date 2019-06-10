a3eb25aeef5f64b7ef8d70a1932f96d2abf62abb
Sam Pearson <sam@sgp.me.uk>
2019-06-10 15:26:46 +0100

    Update default ALLOWED_HOSTS for Django >=1.8.16 compatibility
    Before Django 1.8.16, ALLOWED_HOSTS wasn't checked when DEBUG=True,
    but it is now so the defaults will need to contain our expected
    Host Header.
    
    See: https://docs.djangoproject.com/en/1.8/ref/settings/#allowed-hosts

--- writeit/settings.py
+++ writeit/settings.py
@@ -33,8 +33,9 @@ DATABASES = {
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+# or if not expecting simply ['localhost', '127.0.0.1', '[::1]'].
+# See https://docs.djangoproject.com/en/1.8/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['127.0.0.1.xip.io']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
