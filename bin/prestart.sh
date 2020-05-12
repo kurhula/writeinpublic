4fc615d7aa71a8c1d8f8fa497868c07d40433b94
JD Bothma <jbothma@gmail.com>
2020-05-12 18:02:54 +0200

    WIP compiling stuff on start

--- bin/prestart.sh
+++ bin/prestart.sh
@@ -7,4 +7,5 @@ set -o nounset
 
 python manage.py compilemessages
 python manage.py collectstatic --noinput
-gunicorn  --worker-class gevent zanews.wsgi:application --log-file - --bind 0.0.0.0:5000
+
+exec "$@"
